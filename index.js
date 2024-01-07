505fbb5dcbc7b33df11540261a56b16c5b1c48d6
Brahim64 <bahebrahim2@gmail.com>
2024-01-07 01:06:28 +0100

    Adding mongo database and some GUI components

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express=require('express')
-var app=express()
+var express=require('express');
+var mongoose=require("mongoose");
+const bodyParser = require('body-parser');
+var app=express();
+
+mongoose.connect("mongodb://localhost:27017/test");
+const User = require('./models/User');
 
 app.set('engine view','ejs')
-
-app.use(express.static('public'));//this line help express to know that public have static files and must use them when needed
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
@@ -15,9 +19,53 @@ app.get('/login',(req,res)=>{
 app.get('/signup',(req,res)=>{
     res.render('SignUp.ejs')
 })
-app.get('/profil',(req,res)=>{
+app.post('/profil', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+    
+    if (user) {
+      res.render('profil.ejs',{
+        username:req.body.username,
+        univ:user.univ,
+        profession:user.profession
+      });
+    } else {
+      res.render('login.ejs', { error: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post('/login', async (req, res) => {
+  const { username, password, email, univ, profession } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.render('SignUp.ejs', { error: 'Username is already taken' });
+    }
+
+    const newUser = new User({
+      username,
+      password,
+      email,
+      univ,
+      profession
+    });
+    await newUser.save();
+
+    res.render('login.ejs',{created:"Account created"}); 
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+/*app.get('/profil',(req,res)=>{
     res.render('profil.ejs')
-})
+})*/
 app.listen(8080,()=>{
     console.log("server is running successfully ... ")
 })
